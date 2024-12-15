d9ba3d3f19a36466778a261987d5777d7b8b27b8
crisfa1990 <crisfa1990@gmail.com>
2024-12-15 13:56:46 -0300

    se agregan .json para data de prueba

--- competiciones/models.py
+++ competiciones/models.py
@@ -14,7 +14,7 @@ class Competencia(models.Model):
     nombre = models.CharField(max_length=255)
     tipo = models.CharField(max_length=50, choices=TIPO_COMPETENCIA)
     temporada = models.ForeignKey('equipos.Temporada', on_delete=models.CASCADE, related_name="competencias")
-    divisiones = models.JSONField(null=True, blank=True, help_text="Solo para Liga Local")
+    divisiones = models.IntegerField() 
 
     def __str__(self):
         return self.nombre
