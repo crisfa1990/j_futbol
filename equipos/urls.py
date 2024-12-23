7b2b71307c4f2d6633d7faba4513ce9b2f68bb08
crisfa1990 <crisfa1990@gmail.com>
2024-12-23 00:14:10 -0300

    se agregan templates

--- equipos/urls.py
+++ equipos/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('jugadores/<int:pk>/', views.jugador_detail, name='jugador_detail'),
     path('estadios/', views.estadios_list, name='estadios_list'),
     path('estadios/<int:pk>/', views.estadio_detail, name='estadio_detail'),
+    path('equipos/', views.equipos_list, name='equipos_list'),
+    path('equipos/<int:pk>/', views.equipo_detail, name='equipo_detail'),
+    path('entrenadores/', views.entrenadores_list, name='entrenadores_list'),
+    path('entrenadores/<int:pk>/', views.entrenador_detail, name='entrenador_detail'),
 ]
--- equipos/views.py
+++ equipos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Jugador, Estadio
+from .models import *
 
 def jugadores_list(request):
     jugadores = Jugador.objects.all()
@@ -11,8 +11,24 @@ def jugador_detail(request, pk):
 
 def estadios_list(request):
     estadios = Estadio.objects.all()
-    return render(request, 'estadios_list.html', {'estadios': estadios})
+    return render(request, 'equipos/estadios_list.html', {'estadios': estadios})
 
 def estadio_detail(request, pk):
     estadio = get_object_or_404(Estadio, pk=pk)
-    return render(request, 'estadio_detail.html', {'estadio': estadio})
+    return render(request, 'equipos/estadio_detail.html', {'estadio': estadio})
+
+def equipos_list(request):
+    equipos = Equipo.objects.all()
+    return render(request, 'equipos/equipos_list.html', {'equipos': equipos})
+
+def equipo_detail(request, pk):
+    equipo = get_object_or_404(Equipo, pk=pk)
+    return render(request, 'equipos/equipo_detail.html', {'equipo': equipo})
+
+def entrenadores_list(request):
+    entrenadores = Entrenador.objects.all()
+    return render(request, 'equipos/entrenadores_list.html', {'entrenadores': entrenadores})
+
+def entrenador_detail(request, pk):
+    entrenador = get_object_or_404(Entrenador, pk=pk)
+    return render(request, 'equipos/entrenador_detail.html', {'entrenador': entrenador})
